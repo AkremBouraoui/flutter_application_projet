8bf9b01a705f36f27aeffd7c7cdd4429437a5496
Akrem Bouraoui <bouraoui.akrem.25@hotmail.com>
2025-12-05 17:17:11 -0500

    qjouter la page dshboard

---
+++ lib/dashboard_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'product_details_page.dart';
+import 'product.dart';
+import 'package:flutter_application_ultras/purchase_page.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+   State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  }
+}
