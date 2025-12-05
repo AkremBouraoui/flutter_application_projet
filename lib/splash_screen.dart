390e82e53656d08fd6ec6f80174cfcafbe9eb214
latifMuristaga <abdelatifnemous@gmail.com>
2025-12-05 17:01:43 -0500

    ajouter la structure principale de Homepage

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFF263124),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+    
   }
 }
