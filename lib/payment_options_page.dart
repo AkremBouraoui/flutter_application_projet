833d87d07889f9b5957233322e4f8398082eab92
Amine Kahil <akahil533@gmail.com>
2025-12-05 17:54:39 -0500

    finalisation payment_options

--- lib/payment_options_page.dart
+++ lib/payment_options_page.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-class PaymentOptionsPage extends StatelessWidget {
-  const PaymentOptionsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
