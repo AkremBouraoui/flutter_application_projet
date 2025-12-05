44c1cb07923f527181fbb484598ea2f8a275d532
Akrem Bouraoui <bouraoui.akrem.25@hotmail.com>
2025-12-05 17:18:29 -0500

    ajouter la listes des produit

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -11,5 +11,49 @@ class DashboardPage extends StatelessWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  List<Product> allProducts = [
+    Product(
+      image: "assets/images/buckethat.webp",
+      title: "THE BUCKET HAT",
+      price: "35\$",
+      description:
+          "The bucket hat is a stylish dark green cap with white CSC 1898 embroidery, symbolizing ultras identity and loyalty.",
+    ),
+    Product(
+      image: "assets/images/scarf.webp",
+      title: "SCARF",
+      price: "25\$",
+      description:
+          "A green and black scarf with Ultras Green Army text, showing pride and loyalty in the stands.",
+    ),
+    Product(
+      image: "assets/images/tshirt.webp",
+      title: "T-SHIRT",
+      price: "60\$",
+      description:
+          "Green and black striped design with a white collar, CSC crest on the chest, and 1898 details on the sleeves.",
+    ),
+    Product(
+      image: "assets/images/magazine.webp",
+      title: "MAGAZINE",
+      price: "10\$",
+      description:
+          "Black and green cover with “CSC e Basta”, reflecting ultras passion.",
+    ),
+    Product(
+      image: "assets/images/smokebomb.webp",
+      title: "SMOKE BOMB",
+      price: "15\$",
+      description:
+          "A black canister releasing vibrant green smoke, used by ultras to create atmosphere and show support.",
+    ),
+    Product(
+      image: "assets/images/hoodie.webp",
+      title: "HOODIE",
+      price: "15\$",
+      description:
+          "Black hoodie with the CSC crest on the chest and bold Green Army text.",
+    ),
+  ];
   }
 }
